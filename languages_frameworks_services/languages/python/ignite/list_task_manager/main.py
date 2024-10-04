38276e50d7002b663c14560db404d16bb74aafce
Pablo <pablolucio_@hotmail.com>
2024-10-04 13:59:36 -0300

    feat: adds updateTaskName function to tasks manager python program example

--- languages_frameworks_services/languages/python/ignite/list_task_manager/main.py
+++ languages_frameworks_services/languages/python/ignite/list_task_manager/main.py
@@ -1,3 +1,5 @@
+tasks = []
+
 def addTask(tasksList, taskName="default_task"):
     task = {"name": taskName, "isComplete" : False}
     tasksList.append(task)
@@ -6,13 +8,24 @@ def addTask(tasksList, taskName="default_task"):
 
 
 def listTasks(tasks):
-    print("\n Tasks list:")
-    for index, task in enumerate(tasks, start=1):
-        status = "[✓]" if task["isComplete"] else ""
-        print(f"{index} [{status}] - {task["name"]}")
+    if int(len(tasks)) < 1:
+        print("\nYour tasks list is empty.")
+    else:
+          print("\n Tasks list:")
+          for index, task in enumerate(tasks, start=1):
+            status = "[✓]" if task["isComplete"] else ""
+            print(f"{index} [{status}] - {task["name"]}")
     return
 
-tasks = []
+def updateTaskName(tasks, taskIndex, newTaskName):
+    fixedIndex = int(taskIndex) - 1
+    if fixedIndex >= 0 and fixedIndex < len(tasks):
+        tasks[fixedIndex]["name"] = newTaskName
+        print(f"Task {taskIndex} updated to {newTaskName}")
+    else:
+        print("INdex does not exists.")
+    return
+
 
 while True:
     print("\nWelcome to List tasks manager!")
@@ -32,6 +45,11 @@ while True:
 
     elif userChoice == "2":
         listTasks(tasks)
+
+    elif userChoice == "3":
+        taskIndexInput = input("Type the number of the task you want to update: ")
+        newTaskNameInput = input("Type the new task name: ")
+        updateTaskName(tasks, taskIndexInput, newTaskNameInput)
 
     elif userChoice == "6":
         break
